257aaf34b82d6b46c378607a7da61b76eb60491b
Ap6ykx3 <crilinjavier2121@gmail.com>
2026-02-07 12:13:52 -0400

    Update UniversalHub.lua

--- UniversalHub.lua
+++ UniversalHub.lua
@@ -6,11 +6,10 @@ local GameDatabase = {
         url = "https://raw.githubusercontent.com/Ap6ykx3/AP6-HUB/main/Roll%20In%20A%20Cart%20(SIMPLE%20AUTO%20CASH).lua"
     },
     [6677985923] = {
-        name = "Millonaire Empire Tycoon", 
+        name = "Millionaire Empire Tycoon", 
         url = "https://raw.githubusercontent.com/Ap6ykx3/APHUB-Millonaire-Empire-Tycoon/main/Millonaire%20Empire%20Tycoon.lua",
         onExecute = function()
-            
-            AP6:Notify("MONEY INJECTOR", "1000.0Q+ simulated to your balance.", 5)
+            AP6:Notify("DATA_INJECT", "Generating simulated balance: 1,000.0Q+", 5)
         end
     },
     [130850206307448] = {
