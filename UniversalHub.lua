a26d1e801d6e7e3030e5bebe4b38d9434be86764
Ap6ykx3 <crilinjavier2121@gmail.com>
2026-02-07 12:08:29 -0400

    Update UniversalHub.lua

--- UniversalHub.lua
+++ UniversalHub.lua
@@ -1,21 +1,21 @@
 local AP6 = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ap6ykx3/AP6Library/refs/heads/main/AP6Library.lua"))()
 
 local GameDatabase = {
-    [73904017172892] = {name = "Roll In A Cart", url = "https://raw.githubusercontent.com/Ap6ykx3/AP6-HUB/main/Roll%20In%20A%20Cart%20(SIMPLE%20AUTO%20CASH).lua"},
-    [130850206307448] = {name = "Blox Flipper", url = "https://raw.githubusercontent.com/Ap6ykx3/APHUB-BloxFlipper/main/BloxFlipper.lua"},
-    
-    
+    [73904017172892] = {
+        name = "Roll In A Cart", 
+        url = "https://raw.githubusercontent.com/Ap6ykx3/AP6-HUB/main/Roll%20In%20A%20Cart%20(SIMPLE%20AUTO%20CASH).lua"
+    },
     [6677985923] = {
         name = "Millonaire Empire Tycoon", 
         url = "https://raw.githubusercontent.com/Ap6ykx3/APHUB-Millonaire-Empire-Tycoon/main/Millonaire%20Empire%20Tycoon.lua",
         onExecute = function()
-            
-            AP6:Notify("MONEY INJECTOR", "1000.0Q+ added to your Money Collect", 3)
-            
+            AP6:Notify("MONEY INJECTOR", "1000.0Q+ simulated to your balance.", 5)
         end
     },
-    
-    [120390407164140] = {name = "+$1 Every Correct Glass", url = "https://raw.githubusercontent.com/Ap6ykx3/APHUB--1-000-Every-Collect-Glass/main/EveryCorrectGlass.lua"}
+    [130850206307448] = {
+        name = "Blox Flipper", 
+        url = "https://raw.githubusercontent.com/Ap6ykx3/APHUB-BloxFlipper/main/BloxFlipper.lua"
+    }
 }
 
 AP6:Init(GameDatabase)
