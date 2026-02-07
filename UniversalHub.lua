1fe7ff7e142d01ec303145c19fd8b6f14584f46b
Ap6ykx3 <crilinjavier2121@gmail.com>
2026-02-07 12:27:41 -0400

    Update UniversalHub.lua

--- UniversalHub.lua
+++ UniversalHub.lua
@@ -1,23 +1,22 @@
 local AP6 = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ap6ykx3/AP6Library/refs/heads/main/AP6Library.lua"))()
 
-local GameDatabase = {
+local GameData = {
     [73904017172892] = {
         name = "Roll In A Cart", 
-        url = "https://raw.githubusercontent.com/Ap6ykx3/AP6-HUB/main/Roll%20In%20A%20Cart%20(SIMPLE%20AUTO%20CASH).lua",
-        Maintenance = false
+        url = "https://raw.githubusercontent.com/Ap6ykx3/AP6-HUB/main/Roll%20In%20A%20Cart%20(SIMPLE%20AUTO%20CASH).lua"
     },
     [6677985923] = {
         name = "Millionaire Empire Tycoon", 
         url = "https://raw.githubusercontent.com/Ap6ykx3/APHUB-Millonaire-Empire-Tycoon/main/Millonaire%20Empire%20Tycoon.lua",
         onExecute = function()
-            AP6:Notify("ENGINE", "Script Inyectado Correctamente!", 5)
+            AP6:Notify("INJECTION", "Millionaire Tycoon Scripts Loaded!", 5)
         end
     },
     [130850206307448] = {
         name = "Blox Flipper", 
         url = "https://raw.githubusercontent.com/Ap6ykx3/APHUB-BloxFlipper/main/BloxFlipper.lua",
-        Maintenance = true -- Este saldrá naranja
+        Maintenance = true -- Saldrá con bolita naranja
     }
 }
 
-AP6:Init(GameDatabase)
+AP6:Init(GameData)
